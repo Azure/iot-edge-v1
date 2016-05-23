d65ce8fa856adee0e528d6653afef738f536d2c7
Angelo Ribeiro <aribeironovaes@gmail.com>
2016-05-23 09:50:16 -0700

    DotNET_binding headers and requirements for .NET module host (low level and High Level).

---
+++ bindings/dotnet/inc/dotnet.h
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef DOTNET_H
+#define DOTNET_H
+
+#include "module.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct DOTNET_HOST_CONFIG_TAG
+{
+    const char* dotnet_module_path;
+    const char* dotnet_module_entry_class;
+    const char* dotnet_module_args;
+}DOTNET_HOST_CONFIG;
+
+MODULE_EXPORT const MODULE_APIS* MODULE_STATIC_GETAPIS(DOTNET_HOST)(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*DOTNET_H*/
