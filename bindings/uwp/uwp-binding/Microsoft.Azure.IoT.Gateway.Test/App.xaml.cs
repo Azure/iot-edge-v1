3fe29026bcd9152f2036a2106ed6fd6153a11bf3
Brian Fjeldstad <bfjelds@microsoft.com>
2016-07-11 13:57:39 -0700

    Fix some naming issues for unit tests

--- bindings/uwp/uwp-binding/Microsoft.Azure.IoT.Gateway.Test/App.xaml.cs
+++ bindings/uwp/uwp-binding/Microsoft.Azure.IoT.Gateway.Test/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace UWPTests
+namespace Microsoft.Azure.IoT.Gateway.Test
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- bindings/uwp/uwp-binding/Microsoft.Azure.IoT.Gateway.Test/Properties/AssemblyInfo.cs
+++ bindings/uwp/uwp-binding/Microsoft.Azure.IoT.Gateway.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UWPTests")]
+[assembly: AssemblyTitle("Microsoft.Azure.IoT.Gateway.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UWPTests")]
+[assembly: AssemblyProduct("Microsoft.Azure.IoT.Gateway.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
