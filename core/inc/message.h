17ebab8853452f69aaea253f7af24403607fbb46
darobs <darobs@microsoft.com>
2016-07-12 09:40:12 -0700

    Minor changes to public facing docs.

--- core/inc/message.h
+++ core/inc/message.h
@@ -106,7 +106,7 @@ extern MESSAGE_HANDLE Message_Create(const MESSAGE_CONFIG* cfg);
 *               message and the same content.
 *
 *	@param		source		Pointer to a byte array.
-*               size        size in bytes of the array
+*   @param      size        size in bytes of the array
 *
 *	@return		A non-NULL #MESSAGE_HANDLE for the newly created message, or NULL
 *				upon failure.
@@ -119,7 +119,7 @@ extern MESSAGE_HANDLE Message_CreateFromByteArray(const unsigned char* source, i
 *               to reproduce the message. The returned byte array needs to be deallocated by free function.
 *
 *	@param		messageHandle		A #MESSAGE_HANDLE. This parameter cannot be NULL.
-*               size                pointer to a int32_t that will receive the size of the byte array
+*   @param      size                pointer to a int32_t that will receive the size of the byte array
 *
 *	@return		A non-NULL const unsigned char* is returned when creating the serialized form succeeds. *size is updated to the size of the array
 *               A NULL value is returned when the array cannot be created.
@@ -129,10 +129,12 @@ extern const unsigned char* Message_ToByteArray(MESSAGE_HANDLE messageHandle, in
 
 /** @brief		Creates a new message from a @c CONSTBUFFER source and @c MAP_HANDLE.
 *
-*	@details	This function will create its own @c CONSTBUFFER and @c ConstMap
-*				with copies of the @c source and @c sourceProperties contained
-*				within the #MESSAGE_CONFIG structure parameter. It is the
-*				responsibility of the Message to dispose of these resources.
+*	@details	This function will create a new message its own @c CONSTBUFFER 
+*               and @c ConstMap with copies of the @c source and @c 
+*               sourceProperties contained within the #MESSAGE_CONFIG structure 
+*               parameter. The message will be created with the reference count 
+*               initialized to 1. It is the responsibility of the Message to 
+*               dispose of these resources.
 *
 *	@param		cfg		Pointer to a #MESSAGE_BUFFER_CONFIG structure.
 *
@@ -143,20 +145,20 @@ extern MESSAGE_HANDLE Message_CreateFromBuffer(const MESSAGE_BUFFER_CONFIG* cfg)
 
 /** @brief		Creates a clone of the message.
 *
-*	@details	Since messages are immutable, this function only increments the inner
-                reference count.
+*	@details	Since messages are immutable, this function only increments the 
+*               inner reference count.
 *
 *	@param		message		The #MESSAGE_HANDLE that will be cloned.
 *
-*	@return		A non-NULL #MESSAGE_HANDLE cloned from @c message, or @c NULL upon
-*				failure.
+*	@return		A non-NULL #MESSAGE_HANDLE cloned from @c message, or @c NULL 
+*               upon failure.
 */
 extern MESSAGE_HANDLE Message_Clone(MESSAGE_HANDLE message);
 
 /** @brief		Gets the properties of a message.
 *
-*	@details	The returned @c CONSTMAP handle should be destroyed when no longer
-*				needed.
+*	@details	The returned @c CONSTMAP handle should be destroyed when no 
+*               longer needed.
 *
 *	@param		message		The #MESSAGE_HANDLE from which properties will be
 *							fetched.
@@ -169,26 +171,26 @@ extern CONSTMAP_HANDLE Message_GetProperties(MESSAGE_HANDLE message);
 
 /** @brief		Gets the content of a message.
 *
-*	@details	The returned @c CONSTMAP_HANDLE need not be freed by the caller.
+*	@details	The returned @c CONSTBUFFER need not be freed by the caller.
 *
 *	@param		message		The #MESSAGE_HANDLE from which the content will be
 *							fetched.
 *
-*	@return		A non-NULL pointer to a @c CONSTBUFFER representing the content of 
-*				the message, or @c NULL upon failure.
+*	@return		A non-NULL pointer to a @c CONSTBUFFER representing the content
+*               of the message, or @c NULL upon failure.
 */
 extern const CONSTBUFFER* Message_GetContent(MESSAGE_HANDLE message);
 
-/** @brief		Gets the @c CONSTBUFFER handle that may be used to access the message
-*				content.
+/** @brief		Gets the @c CONSTBUFFER handle that may be used to access the 
+*               message content.
 *
 *	@details	This handle must be destroyed when no longer needed.
 *
 *	@param		message		The #MESSAGE_HANDLE from which the content will be
 *							fetched.
 *							
-*	@return		A non-NULL @c CONSTBUFFER_HANDLE representing the message content, or
-*				@c NULL upon failure.
+*	@return		A non-NULL @c CONSTBUFFER_HANDLE representing the message 
+*               content, or @c NULL upon failure.
 */
 extern CONSTBUFFER_HANDLE Message_GetContentHandle(MESSAGE_HANDLE message);
 
