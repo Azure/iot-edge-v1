7f39d2f1bd44e73cbaa6351ddab7174f9f59b3a6
darobs <darobs@microsoft.com>
2016-09-21 09:33:13 -0700

    Jenkins script setup for java32 This uses newly defined environment vars for java.

--- jenkins/windows_c.cmd
+++ jenkins/windows_c.cmd
@@ -7,14 +7,20 @@ set build-root=%~dp0..
 rem // resolve to fully qualified path
 for %%i in ("%build-root%") do set build-root=%%~fi
 
+REM -- use 32 bit java8 
+set JAVA_SAVE=%JAVA_HOME%
+set JAVA_HOME=%JAVA_8_32%
+
 REM -- C --
 cd %build-root%\tools
-set PATH=%PATH%;%NODE_LIB%
+set PATH=%JAVA_HOME%\bin;%JAVA_HOME%\jre\bin\server;%PATH%;%NODE_LIB%
 
 REM -- Build first dotnet binding for End2End Test.
 call build_dotnet.cmd %*
 if errorlevel 1 goto :eof
 
-call build.cmd --run-e2e-tests --enable-nodejs-binding --enable-dotnet-binding %*
+call build.cmd --run-e2e-tests --enable-nodejs-binding --enable-java-binding --enable-dotnet-binding %*
 if errorlevel 1 goto :eof
 cd %build-root%
+
+set JAVA_HOME=%JAVA_SAVE%
