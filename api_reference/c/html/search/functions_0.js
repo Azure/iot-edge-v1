7f8b757878b5aedc21ce140f43cd2ea44710a4c2
Damon Barry <damonb@microsoft.com>
2017-01-13 17:26:11 -0800

    Documentation for 2017-01-13 release

--- api_reference/c/html/search/functions_0.js
+++ api_reference/c/html/search/functions_0.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['broker_5faddlink',['Broker_AddLink',['../broker_8h.html#a910cb4a93b0c39fbebd55af3020c95d2',1,'broker.h']]],
-  ['broker_5faddmodule',['Broker_AddModule',['../broker_8h.html#ab2fa760ff8d5964a918ca66246d4b043',1,'broker.h']]],
-  ['broker_5fcreate',['Broker_Create',['../broker_8h.html#a6cbd4674f2b3b464d2f0603773d5b398',1,'broker.h']]],
-  ['broker_5fdecref',['Broker_DecRef',['../broker_8h.html#accac1b05e6b48e13e09c185febd9aeba',1,'broker.h']]],
-  ['broker_5fdestroy',['Broker_Destroy',['../broker_8h.html#a52e9eb82b70751aedb66b68aaf5d6703',1,'broker.h']]],
-  ['broker_5fincref',['Broker_IncRef',['../broker_8h.html#a3862c8c206a3cbfd540d28b2ee4b2e6c',1,'broker.h']]],
-  ['broker_5fpublish',['Broker_Publish',['../broker_8h.html#a415ae544429cd27ba52499bf45bc6763',1,'broker.h']]],
-  ['broker_5fremovelink',['Broker_RemoveLink',['../broker_8h.html#a26469622aabda54464b1d4066c966c99',1,'broker.h']]],
-  ['broker_5fremovemodule',['Broker_RemoveModule',['../broker_8h.html#a576edcdaa12f0e2564b4946ccddf5627',1,'broker.h']]]
+  ['broker_5faddlink',['Broker_AddLink',['../broker_8h.html#ab3356bd9be0179bde2ea00952d56f86e',1,'broker.h']]],
+  ['broker_5faddmodule',['Broker_AddModule',['../broker_8h.html#a060b76bcc6215479b715f87d528b8def',1,'broker.h']]],
+  ['broker_5fcreate',['Broker_Create',['../broker_8h.html#a058952a3448e3d5602a141ba20edff6a',1,'broker.h']]],
+  ['broker_5fdecref',['Broker_DecRef',['../broker_8h.html#a65e74a45df222dbd8a64478dce3b6cd6',1,'broker.h']]],
+  ['broker_5fdestroy',['Broker_Destroy',['../broker_8h.html#a249221c2377b24ae0d588b10be3a251a',1,'broker.h']]],
+  ['broker_5fincref',['Broker_IncRef',['../broker_8h.html#a06228f792b1fe162e3f37a5c4d855e6b',1,'broker.h']]],
+  ['broker_5fpublish',['Broker_Publish',['../broker_8h.html#a1f02d0df12cbdaa812a041eda41adcc6',1,'broker.h']]],
+  ['broker_5fremovelink',['Broker_RemoveLink',['../broker_8h.html#aa2c781b0f593d0d895c413542e4036f2',1,'broker.h']]],
+  ['broker_5fremovemodule',['Broker_RemoveModule',['../broker_8h.html#af101c5be54a0b45ea171e296533fc890',1,'broker.h']]]
 ];
--- api_reference/c/html/search/functions_1.js
+++ api_reference/c/html/search/functions_1.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['gateway_5faddlink',['Gateway_AddLink',['../gateway_8h.html#ab64cadb934b7b2337d508acfbfe51e4e',1,'gateway.h']]],
-  ['gateway_5faddmodule',['Gateway_AddModule',['../gateway_8h.html#ae32cd0dcbc1514291c081fd52b553024',1,'gateway.h']]],
-  ['gateway_5fcreate',['Gateway_Create',['../gateway_8h.html#abad51436b772c4839ce7251279f8a920',1,'gateway.h']]],
-  ['gateway_5fcreatefromjson',['Gateway_CreateFromJson',['../gateway_8h.html#ab3081273387aea7f1fbbf5cd774ec7e9',1,'gateway.h']]],
-  ['gateway_5fdestroy',['Gateway_Destroy',['../gateway_8h.html#ace4b910126d464c358d82c55001fa78a',1,'gateway.h']]],
-  ['gateway_5fremovelink',['Gateway_RemoveLink',['../gateway_8h.html#a144f6fd7dc584e7d81d40c05e6da0a20',1,'gateway.h']]],
-  ['gateway_5fremovemodule',['Gateway_RemoveModule',['../gateway_8h.html#aeb7648f3a325383f288981cb41daadbb',1,'gateway.h']]],
-  ['gateway_5fremovemodulebyname',['Gateway_RemoveModuleByName',['../gateway_8h.html#ae4a4ba8e5f81f24dfe227409faa40e26',1,'gateway.h']]],
-  ['gateway_5fstart',['Gateway_Start',['../gateway_8h.html#ab5741590ac72cbfc695ea52ea718641d',1,'gateway.h']]],
-  ['gateway_5fstartmodule',['Gateway_StartModule',['../gateway_8h.html#ac6277fd14abf83835d66a492e2d63e18',1,'gateway.h']]]
+  ['gateway_5faddlink',['Gateway_AddLink',['../gateway_8h.html#afaa43a2033b69d16866a27e01314c09e',1,'gateway.h']]],
+  ['gateway_5faddmodule',['Gateway_AddModule',['../gateway_8h.html#ad27de3704477fae8cb97b860351bf56f',1,'gateway.h']]],
+  ['gateway_5fcreate',['Gateway_Create',['../gateway_8h.html#a8277d807b521bf5513580dddf21b1212',1,'gateway.h']]],
+  ['gateway_5fcreatefromjson',['Gateway_CreateFromJson',['../gateway_8h.html#aa310fdf0ab626e8d068fdea1f8b87ce9',1,'gateway.h']]],
+  ['gateway_5fdestroy',['Gateway_Destroy',['../gateway_8h.html#a53f2b2ce1e62402a67da5df33d95b844',1,'gateway.h']]],
+  ['gateway_5fremovelink',['Gateway_RemoveLink',['../gateway_8h.html#af9d6183899c170ca72a78724615c8a22',1,'gateway.h']]],
+  ['gateway_5fremovemodule',['Gateway_RemoveModule',['../gateway_8h.html#a607e0afa7d4cc46e3d2483c6589dba0d',1,'gateway.h']]],
+  ['gateway_5fremovemodulebyname',['Gateway_RemoveModuleByName',['../gateway_8h.html#a16f456ecc0348ed7eef8d559a534bd11',1,'gateway.h']]],
+  ['gateway_5fstart',['Gateway_Start',['../gateway_8h.html#a2bc0b555e594cd968db3e3d2601fdad2',1,'gateway.h']]],
+  ['gateway_5fstartmodule',['Gateway_StartModule',['../gateway_8h.html#aaab37b00c68fbcb208cd0481c102aa57',1,'gateway.h']]]
 ];
