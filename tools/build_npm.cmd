39a7762187e451214f5da6884084e330573bcd3f
Su Shi <sushi@microsoft.com>
2017-02-08 18:53:55 -0800

    npm: update the pacakge definitions.

--- tools/build_npm.cmd
+++ tools/build_npm.cmd
@@ -1,3 +1,13 @@
 @REM Copyright (c) Microsoft. All rights reserved.
 @REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+@setlocal EnableExtensions EnableDelayedExpansion
+@echo off
+
+set current-path=%~dp0
+
+rem // remove trailing slash
+set current-path=%current-path:~0,-1%
+
+echo %current-path%
+
