3e5d937ed94918561d459f8d656b6dde111cf6cc
Damon Barry <damonb@microsoft.com>
2017-04-12 14:18:44 -0700

    Update gh-pages for the 2017-04-12 release

--- api_reference/dotnetcore/html/menudata.js
+++ api_reference/dotnetcore/html/menudata.js
@@ -1,11 +1,11 @@
 var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Namespaces",url:"namespaces.html",children:[
-{text:"Namespace List",url:"namespaces.html"}]},
-{text:"Classes",url:"annotated.html",children:[
-{text:"Class List",url:"annotated.html"},
-{text:"Class Index",url:"classes.html"},
-{text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html"},
-{text:"Functions",url:"functions_func.html"},
-{text:"Properties",url:"functions_prop.html"}]}]}]}
+{text:'Main Page',url:'index.html'},
+{text:'Namespaces',url:'namespaces.html',children:[
+{text:'Namespace List',url:'namespaces.html'}]},
+{text:'Classes',url:'annotated.html',children:[
+{text:'Class List',url:'annotated.html'},
+{text:'Class Index',url:'classes.html'},
+{text:'Class Members',url:'functions.html',children:[
+{text:'All',url:'functions.html'},
+{text:'Functions',url:'functions_func.html'},
+{text:'Properties',url:'functions_prop.html'}]}]}]}
