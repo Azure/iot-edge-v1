ea53f8116f41e301b7d723877cb2b79427630232
darobs <darobs@microsoft.com>
2017-08-22 10:21:37 -0700

    Azure IoT Gateway SDK 2017-08-21 release

--- api_reference/c/html/menudata.js
+++ api_reference/c/html/menudata.js
@@ -1,57 +1,57 @@
 var menudata={children:[
-{text:'Main Page',url:'index.html'},
-{text:'Data Structures',url:'annotated.html',children:[
-{text:'Data Structures',url:'annotated.html'},
-{text:'Data Structure Index',url:'classes.html'},
-{text:'Data Fields',url:'functions.html',children:[
-{text:'All',url:'functions.html',children:[
-{text:'a',url:'functions.html#index_a'},
-{text:'b',url:'functions.html#index_b'},
-{text:'c',url:'functions.html#index_c'},
-{text:'e',url:'functions.html#index_e'},
-{text:'f',url:'functions.html#index_f'},
-{text:'g',url:'functions.html#index_g'},
-{text:'l',url:'functions.html#index_l'},
-{text:'m',url:'functions.html#index_m'},
-{text:'n',url:'functions.html#index_n'},
-{text:'p',url:'functions.html#index_p'},
-{text:'s',url:'functions.html#index_s'},
-{text:'t',url:'functions.html#index_t'},
-{text:'u',url:'functions.html#index_u'},
-{text:'v',url:'functions.html#index_v'}]},
-{text:'Variables',url:'functions_vars.html',children:[
-{text:'a',url:'functions_vars.html#index_a'},
-{text:'b',url:'functions_vars.html#index_b'},
-{text:'c',url:'functions_vars.html#index_c'},
-{text:'e',url:'functions_vars.html#index_e'},
-{text:'f',url:'functions_vars.html#index_f'},
-{text:'g',url:'functions_vars.html#index_g'},
-{text:'l',url:'functions_vars.html#index_l'},
-{text:'m',url:'functions_vars.html#index_m'},
-{text:'n',url:'functions_vars.html#index_n'},
-{text:'p',url:'functions_vars.html#index_p'},
-{text:'s',url:'functions_vars.html#index_s'},
-{text:'t',url:'functions_vars.html#index_t'},
-{text:'u',url:'functions_vars.html#index_u'},
-{text:'v',url:'functions_vars.html#index_v'}]}]}]},
-{text:'Files',url:'files.html',children:[
-{text:'File List',url:'files.html'},
-{text:'Globals',url:'globals.html',children:[
-{text:'All',url:'globals.html',children:[
-{text:'b',url:'globals.html#index_b'},
-{text:'d',url:'globals.html#index_d'},
-{text:'g',url:'globals.html#index_g'},
-{text:'m',url:'globals.html#index_m'},
-{text:'p',url:'globals.html#index_p'}]},
-{text:'Functions',url:'globals_func.html',children:[
-{text:'b',url:'globals_func.html#index_b'},
-{text:'g',url:'globals_func.html#index_g'},
-{text:'m',url:'globals_func.html#index_m'}]},
-{text:'Typedefs',url:'globals_type.html',children:[
-{text:'b',url:'globals_type.html#index_b'},
-{text:'d',url:'globals_type.html#index_d'},
-{text:'g',url:'globals_type.html#index_g'},
-{text:'m',url:'globals_type.html#index_m'},
-{text:'p',url:'globals_type.html#index_p'}]},
-{text:'Enumerations',url:'globals_enum.html'},
-{text:'Macros',url:'globals_defs.html'}]}]}]}
+{text:"Main Page",url:"index.html"},
+{text:"Data Structures",url:"annotated.html",children:[
+{text:"Data Structures",url:"annotated.html"},
+{text:"Data Structure Index",url:"classes.html"},
+{text:"Data Fields",url:"functions.html",children:[
+{text:"All",url:"functions.html",children:[
+{text:"a",url:"functions.html#index_a"},
+{text:"b",url:"functions.html#index_b"},
+{text:"c",url:"functions.html#index_c"},
+{text:"e",url:"functions.html#index_e"},
+{text:"f",url:"functions.html#index_f"},
+{text:"g",url:"functions.html#index_g"},
+{text:"l",url:"functions.html#index_l"},
+{text:"m",url:"functions.html#index_m"},
+{text:"n",url:"functions.html#index_n"},
+{text:"p",url:"functions.html#index_p"},
+{text:"s",url:"functions.html#index_s"},
+{text:"t",url:"functions.html#index_t"},
+{text:"u",url:"functions.html#index_u"},
+{text:"v",url:"functions.html#index_v"}]},
+{text:"Variables",url:"functions_vars.html",children:[
+{text:"a",url:"functions_vars.html#index_a"},
+{text:"b",url:"functions_vars.html#index_b"},
+{text:"c",url:"functions_vars.html#index_c"},
+{text:"e",url:"functions_vars.html#index_e"},
+{text:"f",url:"functions_vars.html#index_f"},
+{text:"g",url:"functions_vars.html#index_g"},
+{text:"l",url:"functions_vars.html#index_l"},
+{text:"m",url:"functions_vars.html#index_m"},
+{text:"n",url:"functions_vars.html#index_n"},
+{text:"p",url:"functions_vars.html#index_p"},
+{text:"s",url:"functions_vars.html#index_s"},
+{text:"t",url:"functions_vars.html#index_t"},
+{text:"u",url:"functions_vars.html#index_u"},
+{text:"v",url:"functions_vars.html#index_v"}]}]}]},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"},
+{text:"Globals",url:"globals.html",children:[
+{text:"All",url:"globals.html",children:[
+{text:"b",url:"globals.html#index_b"},
+{text:"d",url:"globals.html#index_d"},
+{text:"g",url:"globals.html#index_g"},
+{text:"m",url:"globals.html#index_m"},
+{text:"p",url:"globals.html#index_p"}]},
+{text:"Functions",url:"globals_func.html",children:[
+{text:"b",url:"globals_func.html#index_b"},
+{text:"g",url:"globals_func.html#index_g"},
+{text:"m",url:"globals_func.html#index_m"}]},
+{text:"Typedefs",url:"globals_type.html",children:[
+{text:"b",url:"globals_type.html#index_b"},
+{text:"d",url:"globals_type.html#index_d"},
+{text:"g",url:"globals_type.html#index_g"},
+{text:"m",url:"globals_type.html#index_m"},
+{text:"p",url:"globals_type.html#index_p"}]},
+{text:"Enumerations",url:"globals_enum.html"},
+{text:"Macros",url:"globals_defs.html"}]}]}]}
