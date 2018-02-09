a80857e05be0dc4bbc2de93555e6f83700c3d887
Dariusz Parys <downtocode@gmail.com>
2018-02-09 12:27:39 +0100

    V2 Sample Simulated Temperature Sensor
    Sample code for Iot Edge V2

---
+++ v2/samples/azureiotedge-simulated-temperature-sensor/Program.cs
@@ -0,0 +1,198 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace azureiotedge_simulated_temperature_sensor
+{
+    class Program
+    {
+        static int counter;
+
+        static DesiredProperties desiredProperties;
+        static DataGenerationPolicy generationPolicy = new DataGenerationPolicy();
+
+        private static volatile bool IsReset = false;
+
+        static async Task Main(string[] args)
+        {
+            // The Edge runtime gives us the connection string we need -- it is injected as an environment variable
+            var connectionString = Environment.GetEnvironmentVariable("EdgeHubConnectionString");
+
+            // Cert verification is not yet fully functional when using Windows OS for the container
+            var bypassCertVerification = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            if (!bypassCertVerification) InstallCert();
+            await Init(connectionString, bypassCertVerification);
+
+            // Wait until the app unloads or is cancelled
+            var cts = new CancellationTokenSource();
+            AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
+            Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
+            await WhenCancelled(cts.Token);
+        }
+
+        /// <summary>
+        /// Handles cleanup operations when app is cancelled or unloads
+        /// </summary>
+        public static Task WhenCancelled(CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Add certificate in local cert store for use by client for secure connection to IoT Edge runtime
+        /// </summary>
+        static void InstallCert()
+        {
+            var certPath = Environment.GetEnvironmentVariable("EdgeModuleCACertificateFile");
+            if (string.IsNullOrWhiteSpace(certPath))
+            {
+                // We cannot proceed further without a proper cert file
+                Console.WriteLine($"Missing path to certificate collection file: {certPath}");
+                throw new InvalidOperationException("Missing path to certificate file.");
+            }
+            else if (!File.Exists(certPath))
+            {
+                // We cannot proceed further without a proper cert file
+                Console.WriteLine($"Missing path to certificate collection file: {certPath}");
+                throw new InvalidOperationException("Missing certificate file.");
+            }
+            var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(new X509Certificate2(X509Certificate2.CreateFromCertFile(certPath)));
+            Console.WriteLine("Added Cert: " + certPath);
+            store.Close();
+        }
+
+
+        /// <summary>
+        /// Initializes the DeviceClient and sets up the callback to receive
+        /// messages containing temperature information
+        /// </summary>
+        static async Task Init(string connectionString, bool bypassCertVerification = false)
+        {
+            Console.WriteLine("Connection String {0}", connectionString);
+
+            var mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
+            // During dev you might want to bypass the cert verification. It is highly recommended to verify certs systematically in production
+            if (bypassCertVerification)
+            {
+                mqttSetting.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+            }
+            ITransportSettings[] settings = { mqttSetting };
+
+            // Open a connection to the Edge runtime
+            var ioTHubModuleClient = DeviceClient.CreateFromConnectionString(connectionString, settings);
+            await ioTHubModuleClient.OpenAsync();
+            Console.WriteLine("IoT Hub module client initialized.");
+
+            var moduleTwin = await ioTHubModuleClient.GetTwinAsync();
+            var moduleTwinCollection = moduleTwin.Properties.Desired;
+            desiredProperties = new DesiredProperties();
+            desiredProperties.UpdateDesiredProperties(moduleTwinCollection);
+
+            // callback for updating desired properties through the portal or rest api
+            await ioTHubModuleClient.SetDesiredPropertyUpdateCallbackAsync(onDesiredPropertiesUpdate, null);
+
+            // this direct method will allow to reset the temperature sensor values back to their initial state
+            await ioTHubModuleClient.SetMethodHandlerAsync("reset", ResetMethod, null);
+
+            // we don't pass ioTHubModuleClient as we're not sending any messages out to the message bus
+            await ioTHubModuleClient.SetInputMessageHandlerAsync("control", ControlMessageHandler, null);
+
+            while(true)
+            {
+                try
+                {
+                    if(desiredProperties.SendData)
+                    {
+                        counter++;
+                        if(counter == 1)
+                        {
+                            // first time execution needs to reset the data factory
+                            IsReset = true;
+                        }
+
+                        var messageBody = TemperatureDataFactory.CreateTemperatureData(counter, generationPolicy, IsReset);
+                        IsReset = false;
+                        var messageString = JsonConvert.SerializeObject(messageBody);
+                        var messageBytes = Encoding.UTF8.GetBytes(messageString);
+                        var message = new Message(messageBytes);
+
+                        await ioTHubModuleClient.SendEventAsync("temperatureOutput", message);
+                        Console.WriteLine($"\t{DateTime.UtcNow.ToShortDateString()} {DateTime.UtcNow.ToLongTimeString()}> Sending message: {counter}, Body: {messageString}");
+
+                    }
+                    await Task.Delay(TimeSpan.FromSeconds(desiredProperties.SendInterval));
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"[ERROR] Unexpected Exception {ex.Message}" );
+                    Console.WriteLine($"\t{ex.ToString()}");
+                }
+            }
+        }
+
+        private static Task onDesiredPropertiesUpdate(TwinCollection twinCollection, object userContext)
+        {
+            desiredProperties.UpdateDesiredProperties(twinCollection);
+            return Task.CompletedTask;
+        }
+
+        private static Task<MethodResponse> ResetMethod(MethodRequest request, object userContext)
+        {
+            var response = new MethodResponse((int) HttpStatusCode.OK);
+            Console.WriteLine("Received reset command via direct method invocation");
+            Console.WriteLine("Resetting temperature sensor...");
+            IsReset = true;
+            return Task.FromResult(response);
+        }
+
+        private static Task<MessageResponse> ControlMessageHandler(Message message, object userContext)
+        {
+            var messageBytes = message.GetBytes();
+            var messageString = Encoding.UTF8.GetString(messageBytes);
+
+            Console.WriteLine($"Received message Body: [{messageString}]");
+
+            try
+            {
+                var messages = JsonConvert.DeserializeObject<ControlCommand[]>(messageString);
+                foreach (ControlCommand messageBody in messages)
+                {
+                    if (messageBody.Command == ControlCommandEnum.Reset)
+                    {
+                        Console.WriteLine("Resetting temperature sensor..");
+                        IsReset = true;
+                    }
+                    else
+                    {
+                        //NoOp
+                        Console.WriteLine("Received NOOP message");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to deserialize control command with exception: [{ex.Message}]");
+            }
+
+            return Task.FromResult(MessageResponse.Completed);
+        }
+    }
+}
