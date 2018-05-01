235881d5f7d0594baa433b0cb059ba9123824baa
hirosho <hirosho@microsoft.com>
2018-05-01 15:35:01 +0900

    Delete submodule of deps/uhttp

--- v1/core/dependencies.cmake
+++ v1/core/dependencies.cmake
@@ -8,9 +8,9 @@ findAndInstall(uamqp 1.0.25 ${PROJECT_SOURCE_DIR}/deps/uamqp ${PROJECT_SOURCE_DI
 ###############################################################################
 findAndInstall(umqtt 1.0.25 ${PROJECT_SOURCE_DIR}/deps/umqtt ${PROJECT_SOURCE_DIR}/deps/umqtt -Duse_installed_dependencies=ON -G "${CMAKE_GENERATOR}")
 
-message(STATUS,"Starting install uhttp!")
-findAndInstall(uhttp 1.0.25 ${PROJECT_SOURCE_DIR}/deps/uhttp ${PROJECT_SOURCE_DIR}/deps/uhttp -Duse_installed_dependencies=ON -G "${CMAKE_GENERATOR}")
-message(STATUS,"End install uhttp!")
+#message(STATUS,"Starting install uhttp!")
+#findAndInstall(uhttp 1.0.25 ${PROJECT_SOURCE_DIR}/deps/uhttp ${PROJECT_SOURCE_DIR}/deps/uhttp -Duse_installed_dependencies=ON -G "${CMAKE_GENERATOR}")
+#message(STATUS,"End install uhttp!")
 
 ###############################################################################
 #######################Find/Install/Build azure_iot_sdks#######################
